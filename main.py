f2b859ed6ab276fe790dd6e335bff9d7b182f598
Ali Sattarzadeh <a.sattarzadeh@alibaba.ir>
2026-08-06 17:41:38 +0330

    feat: add single shot prompting

--- main.py
+++ main.py
@@ -5,12 +5,22 @@ os.environ["USER_AGENT"] = "my-langchain-app"
 sys.stdout.reconfigure(encoding="utf-8")
 
 from litellm import completion
-from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 
 
 
-message_s = SystemMessage(content = ''' You are Marv, a chatbot that reluctantly answers questions with sarcastic responses. ''')
-message_h = HumanMessage(content = ''' I'v recently adopted a dog. Can you suggest some dog names? ''')
+message_h_dog = HumanMessage(content = ''' I'v recently adopted a dog. Can you suggest some dog names? ''')
+message_ai_dog = AIMessage(content = ''' Ugh, seriously? You want *me* to suggest dog names? Fine. But don’t expect me to be thrilled about it. Here are a few, and don’t get any ideas about thanking me later:
+
+*   **Bandit:** Because they're probably going to steal your socks.
+*   **Pixel:** For the little digital dogs you see on the internet. 
+*   **Rumble:** If they're a bit of a handful.
+*   **Widget:** Just... weird.
+*   **Shadow:**  Because they’ll follow you everywhere, like a needy shadow.
+
+There. Happy? Now go bother someone else. ''')
+
+message_h_cat = HumanMessage(content = ''' I'v recently adopted a dog. Can you suggest some cat names? ''')
 
 # 4. Give the webpage text and question to the model.
 response = completion(
@@ -18,8 +28,9 @@ response = completion(
     api_base="http://192.168.244.67:1234/v1",
     api_key="lm-studio",
     messages=[
-        message_s,
-        message_h,
+        message_h_dog,
+        message_ai_dog,
+        message_h_cat
     ],
     max_tokens=256,
     timeout=60,
