18fabbe27d001bdc4a2a28c20bbc247bd3bd46c2
Ali Sattarzadeh <a.sattarzadeh@alibaba.ir>
2026-08-06 17:31:30 +0330

    feat: use SystemMessage from langchain as an example

--- main.py
+++ main.py
@@ -4,43 +4,13 @@ import sys
 os.environ["USER_AGENT"] = "my-langchain-app"
 sys.stdout.reconfigure(encoding="utf-8")
 
-from langchain_community.document_loaders import WebBaseLoader
 from litellm import completion
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
-
-# 1. Choose the webpage containing your custom data.
-URL = "https://vakilnoor.ir/posts"
-
-# 2. Load the webpage as a LangChain document.
-raw_documents = WebBaseLoader(URL).load()
-text_splitter = RecursiveCharacterTextSplitter()
-documents = text_splitter.split_documents(raw_documents)
-
-embeddings = OpenAIEmbeddings(
-    model="text-embedding-nomic-embed-text-v1.5",
-    base_url="http://192.168.244.67:1234/v1",
-    api_key="lm-studio",
-    check_embedding_ctx_length=False,
-)
-
-from langchain_core.vectorstores import InMemoryVectorStore
-
-vector_store = InMemoryVectorStore.from_documents(
-    documents=documents,
-    embedding=embeddings,
-)
+from langchain_core.messages import SystemMessage, HumanMessage
 
 
-question = "what is latest post of vakilnoor website?"
 
-
-results = vector_store.similarity_search(question, k=3)
-
-webpage_text = "\n\n".join(
-    document.page_content for document in results
-)
-# 3. Ask a question about that data.
+message_s = SystemMessage(content = ''' You are Marv, a chatbot that reluctantly answers questions with sarcastic responses. ''')
+message_h = HumanMessage(content = ''' I'v recently adopted a dog. Can you suggest some dog names? ''')
 
 # 4. Give the webpage text and question to the model.
 response = completion(
@@ -48,14 +18,8 @@ response = completion(
     api_base="http://192.168.244.67:1234/v1",
     api_key="lm-studio",
     messages=[
-        {
-            "role": "system",
-            "content": "Answer only from the provided webpage content.",
-        },
-        {
-            "role": "user",
-            "content": f"Webpage content:\n{webpage_text}\n\nQuestion: {question}",
-        },
+        message_s,
+        message_h,
     ],
     max_tokens=256,
     timeout=60,
