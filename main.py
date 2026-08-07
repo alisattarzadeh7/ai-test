6568eb042aaf5880a90dcf82769790731da1b1ea
Ali Sattarzadeh <a.sattarzadeh@alibaba.ir>
2026-08-07 21:33:24 +0330

    feat: add batching method

--- main.py
+++ main.py
@@ -1,33 +1,58 @@
 import os
 import sys
 
-from langchain_classic.output_parsers import DatetimeOutputParser
-from langchain_core.output_parsers import CommaSeparatedListOutputParser
-
 os.environ["USER_AGENT"] = "my-langchain-app"
 sys.stdout.reconfigure(encoding="utf-8")
 
 from litellm import completion
-from langchain_core.messages import HumanMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableLambda
+
+chat_template = ChatPromptTemplate.from_messages([
+    (
+        "human",
+        "I've recently adopted a {pet} which is a {breed}. "
+        "Could you suggest training tips?"
+    )
+])
 
 
-message_h = HumanMessage(content=f'''  when was chatgpt launched?
-    {DatetimeOutputParser().get_format_instructions()}
+def call_llm(prompt_value):
+    # Convert LangChain messages -> OpenAI/LiteLLM format
+    messages = [
+        {
+            "role": "user" if msg.type == "human" else msg.type,
+            "content": msg.content
+        }
+        for msg in prompt_value.to_messages()
+    ]
 
-''')
+    response = completion(
+        model="openai/google/gemma-3-4b",
+        api_base="http://192.168.244.67:1234/v1",
+        api_key="lm-studio",
+        messages=messages,
+        max_tokens=256,
+        timeout=60,
+    )
 
-print(message_h.content)
+    return response.choices[0].message.content
 
-# # 4. Give the webpage text and question to the model.
-response = completion(
-    model="openai/google/gemma-3-4b",
-    api_base="http://192.168.244.67:1234/v1",
-    api_key="lm-studio",
-    messages= [message_h],
-    max_tokens=256,
-    timeout=60,
-)
 
-date_output_parser  = DatetimeOutputParser()
-response_parsed = date_output_parser.invoke(response.choices[0].message.content)
-print(response_parsed)
+llm = RunnableLambda(call_llm)
+
+chain = chat_template | llm
+
+
+inputs = [
+    {"pet": "dog", "breed": "Labrador"},
+    {"pet": "dog", "breed": "German Shepherd"},
+    {"pet": "cat", "breed": "Siamese"},
+    {"pet": "dog", "breed": "Golden Retriever"},
+]
+
+responses = chain.batch(inputs)
+
+for input_data, response in zip(inputs, responses):
+    print(f"\n{input_data['pet']} - {input_data['breed']}")
+    print(response)
