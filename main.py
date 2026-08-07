8f7b30c4614d51b1545d06877386f83199b8685b
Ali Sattarzadeh <a.sattarzadeh@alibaba.ir>
2026-08-07 21:17:04 +0330

    feat: add date time parser

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+from langchain_classic.output_parsers import DatetimeOutputParser
 from langchain_core.output_parsers import CommaSeparatedListOutputParser
 
 os.environ["USER_AGENT"] = "my-langchain-app"
@@ -10,13 +11,14 @@ from litellm import completion
 from langchain_core.messages import HumanMessage
 
 
-message_h = HumanMessage(content=f'''  I've recently adopted a dog. Could you suggest some dog names?  
-    {CommaSeparatedListOutputParser().get_format_instructions()}
+message_h = HumanMessage(content=f'''  when was chatgpt launched?
+    {DatetimeOutputParser().get_format_instructions()}
+
 ''')
 
 print(message_h.content)
 
-# 4. Give the webpage text and question to the model.
+# # 4. Give the webpage text and question to the model.
 response = completion(
     model="openai/google/gemma-3-4b",
     api_base="http://192.168.244.67:1234/v1",
@@ -26,8 +28,6 @@ response = completion(
     timeout=60,
 )
 
-list_output_parser  = CommaSeparatedListOutputParser()
-response_parsed = list_output_parser.invoke(response.choices[0].message.content)
-
-# 5. Print the answer.
+date_output_parser  = DatetimeOutputParser()
+response_parsed = date_output_parser.invoke(response.choices[0].message.content)
 print(response_parsed)
