06e94d5cc1a319e3edd431c40dd680ea84380129
Ali Sattarzadeh <a.sattarzadeh@alibaba.ir>
2026-08-13 06:03:32 +0330

    feat: retrieve from database using similarity search

--- main.py
+++ main.py
@@ -1,31 +1,6 @@
-from langchain_community.document_loaders import Docx2txtLoader
 from langchain_openai import OpenAIEmbeddings
 
-from langchain_community.vectorstores import Chroma
-from langchain_text_splitters import CharacterTextSplitter, MarkdownHeaderTextSplitter
-
-
-loader = Docx2txtLoader('Introduction_to_Data_and_Data_Science_2.docx')
-
-pages = loader.load()
-
-
-md_splitter = MarkdownHeaderTextSplitter(headers_to_split_on= [("#","Course Title"),("##","Lecture title")])
-
-pages_md_split = md_splitter.split_text(pages[0].page_content)
-
-
-
-
-for i in range(len(pages_md_split)):
-    pages_md_split[i].page_content = " ".join(pages_md_split[i].page_content.split())
-
-
-char_splitter = CharacterTextSplitter(separator=".", chunk_size= 500, chunk_overlap=0)
-
-page_char_split = char_splitter.split_documents(pages_md_split)
-
-text = page_char_split[3].page_content
+from langchain_chroma import Chroma
 
 embeddings = OpenAIEmbeddings(
     model="text-embedding-mxbai-embed-large-v1",
@@ -34,10 +9,11 @@ embeddings = OpenAIEmbeddings(
     check_embedding_ctx_length=False,  # Important for LM Studio
 )
 
-vector_store = Chroma.from_documents(
-    documents=page_char_split,
-    embedding=embeddings,
-    collection_name="data_science_course",
-    persist_directory="./chroma_db",
-)
+vectorstore = Chroma(collection_name="data_science_course",persist_directory='./chroma_db',embedding_function=embeddings)
+
+
+question  = "What programming languages do data scientists use?"
+retrieved_docs = vectorstore.similarity_search(query=question,k = 5)
+
+print(retrieved_docs)
 
