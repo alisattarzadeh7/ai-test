a3e10c98dcd327df59a24881fa49ccdd7bdf0081
Ali Sattarzadeh <a.sattarzadeh@alibaba.ir>
2026-08-14 04:24:33 +0330

    feat: add message for keeping conversation

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-from typing import TypedDict, Sequence, Literal
+from typing import TypedDict, Sequence, Literal, Annotated
 
-from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_core.messages import BaseMessage, HumanMessage, AIMessage, RemoveMessage
 from langchain_openai import ChatOpenAI
-from langgraph.graph import StateGraph,START,END
-
-
-
-
-class State(TypedDict):
-    messages: Sequence[BaseMessage]
-
-
-state = State(messages=[HumanMessage("Could you tell me a grook by Piet Hein?")])
+from langgraph.graph import StateGraph, START, END, add_messages, MessagesState
 
 
 
@@ -23,53 +14,63 @@ chat = ChatOpenAI(
     temperature=0.7,
 )
 
-response  = chat.invoke(state["messages"])
 
-
-def ask_question(state: State) -> State:
+def ask_question(state: MessagesState) -> MessagesState:
     print(f"\n ----> ENTERING ask_question:")
-
-    print("what is your question?")
-    return State(messages=[HumanMessage(input())])
-
-
-def ask_another_question(state: State) -> State:
-    print(f"\n ----> ENTERING ask_another_question:")
-
-    print("Whould you like to ask another question?")
-    return State(messages=[HumanMessage(input())])
+    for i in state["messages"]:
+        i.pretty_print()
+    question = "what is your question?"
+    print(question)
+    return MessagesState(messages=[HumanMessage(input())])
 
 
 
-def chatbot(state:State)->State:
+def chatbot(state:MessagesState)->MessagesState:
     print(f"\n ----> ENTERING chatbot:")
     response  = chat.invoke(state["messages"])
-    print(response.content)
-    return  State(messages= [response])
+    print(response.pretty_print())
+    return  MessagesState(messages= [response])
 
 
-def routing_function(state:State)-> Literal["ask_question","__end__"]:
+def ask_another_question(state: MessagesState) -> MessagesState:
+    print(f"\n ----> ENTERING ask_another_question:")
+
+    question = "Whould you like to ask another question?"
+    print(question)
+    return MessagesState(messages=[HumanMessage(input())])
+
+
+
+
+def routing_function(state:MessagesState)-> Literal["trim_messages","__end__"]:
     print(f"\n ----> ENTERING routing_function:")
-    if state["messages"][0].content == "yes":
-        return "ask_question"
+    if state["messages"][-1].content == "yes":
+        return "trim_messages"
     else:
         return "__end__"
 
 
+def trim_messages(state:MessagesState)->MessagesState:
+    print(f"\n ----> ENTERING trim_messages:")
+    remove_messages = [RemoveMessage(id = i.id) for i in state["messages"][:-5]]
 
-graph = StateGraph(State)
+    return MessagesState(messages=remove_messages)
+
+
+
+graph = StateGraph(MessagesState)
 
 graph.add_node("ask_question",ask_question)
 graph.add_node("chatbot",chatbot)
 graph.add_node("ask_another_question",ask_another_question)
+graph.add_node("trim_messages",trim_messages)
 
 graph.add_edge(START,"ask_question")
 graph.add_edge("ask_question","chatbot")
 graph.add_edge("chatbot","ask_another_question")
-graph.add_conditional_edges(source="ask_another_question",path = routing_function,path_map= {
-        "ask_question": "ask_question",
-        "__end__": "__end__"
-    })
+graph.add_conditional_edges(source="ask_another_question",path = routing_function)
+
+graph.add_edge("trim_messages","ask_question")
 
 graph_compiled = graph.compile()
 
@@ -77,7 +78,7 @@ graph_compiled = graph.compile()
 # print(graph_compiled.get_graph().draw_ascii())
 
 
-graph_compiled.invoke(State(messages=[]))
+graph_compiled.invoke(MessagesState(messages=[]))
 
 
 
